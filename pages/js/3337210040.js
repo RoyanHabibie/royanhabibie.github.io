66ebd0a239f253b7f79b32756d9736b655fc1799
Anjeli Gusnawan <116627857+Anjeellun@users.noreply.github.com>
2022-10-27 23:49:24 +0700

    3337210040

---
+++ pages/js/3337210040.js
@@ -0,0 +1,28 @@
+
+//JS Bootsrap
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    //Activate Bootstrap scroll
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            offset: 74,
+        });
+    };
+
+    //Responsive navbar 
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
