e927c1b77966c5b29276336a265343874a35dd48
rhsukarna@gmail.com <RoyanHabibie>
2022-11-08 15:21:23 +0700

    upadte

--- profile.php
+++ profile.php
@@ -62,7 +62,7 @@ $awards = mysqli_query($conn, "SELECT * FROM awards WHERE nim=$nim");
             <ul class="navbar-nav">
                 <li class="nav-item"><a class="nav-link js-scroll-trigger" href="index.php">Home</a></li>
                 <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">About</a></li>
-                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#education">Education</a></li>
+                <!-- <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#education">Education</a></li> -->
                 <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#skills">Skills</a></li>
                 <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#interests">Interests</a></li>
                 <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#awards">Awards</a></li>
